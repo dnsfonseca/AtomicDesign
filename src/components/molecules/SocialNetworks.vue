76dc930cc99a374d0c6685a37d0b8858d40e97d8
dnsfonseca <daniel-fonseca13@hotmail.com>
2022-02-19 17:15:46 -0300

    Enviando o projeto para o GitHub

---
+++ src/components/molecules/SocialNetworks.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="social-networks">
+    <SocialLink
+      name="Github"
+      to="https://github.com/dnsfonseca"
+      icon="github.svg"
+      target="_blank"
+    />
+    <SocialLink
+      name="Twitter"
+      to="https://twitter.com"
+      icon="twitter.svg"
+      target="_blank"
+    />
+    <SocialLink
+      name="Facebook"
+      to="https://facebook.com"
+      icon="facebook.svg"
+      target="_blank"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { SocialLink } from '@/components/atoms'
+
+export default defineComponent({
+  components: { SocialLink }
+})
+</script>
+
+<style lang="scss" scoped>
+.social-networks {
+  display: grid;
+  grid-gap: 1rem;
+}
+</style>
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="services">
+    <Service
+      title="Front End"
+      description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate vero voluptatibus corrupti dignissimos dicta commodi modi, rem cumque distinctio, quaerat eum tempora ad iste repudiandae consequatur quis excepturi et fugit!"
+    />
+    <Service
+      title="Back End"
+      description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate vero voluptatibus corrupti dignissimos dicta commodi modi, rem cumque distinctio, quaerat eum tempora ad iste repudiandae consequatur quis excepturi et fugit!"
+    />
+    <Service
+      title="Manutenção"
+      description="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate vero voluptatibus corrupti dignissimos dicta commodi modi, rem cumque distinctio, quaerat eum tempora ad iste repudiandae consequatur quis excepturi et fugit!"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Service } from '@/components/molecules'
+
+export default defineComponent({
+  components: { Service }
+})
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
